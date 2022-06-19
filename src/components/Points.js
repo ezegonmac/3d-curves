48f921e63997f503696aaf3be38b8d7654a0bffb
EGM <ezegonmac01@gmail.com>
2022-06-19 23:55:07 +0200

    Added Sphere geometry

--- src/components/Points.js
+++ src/components/Points.js
@@ -3,15 +3,15 @@ import * as THREE from 'three';
 const Points = () => {
 
     const positions = new Float32Array(
-        [-5,0,0, 
-          2,2,2, 
-          1,1,1,
-          5,0,0]);
+        [1,0,0, 
+          0,1,0, 
+          0,0,1,
+          0,0,0]);
     const colors = new Float32Array(
-        [1,0.5,0.5,
-         1,0.5,0.5,
-         1,0.5,0.5,
-         1,0.5,0.5]);
+        [1,0,0,
+         0,1,0,
+         0,0,1,
+         1,1,1]);
 
     return (
         <points>
